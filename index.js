959cbfe5a970281c05a518f058cfe0a2b61d6a88
Sebastian Brown <SBrown@forsythesit.com.au>
2019-10-30 12:42:00 +1100

    Added rest call

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const io = require('@actions/io');
 const fs = require('fs');
 
 
 try {
     const projectName = core.getInput('project-name');
     const gistId = core.getInput('gist-id');
+    const gitHubUser = core.getInput('github-user');
     const accessToken = core.getInput('access-token');
     const coverageSummary = core.getInput('coverage-summary');
 
     console.log(projectName, gistId, coverageSummary);
 
     // First I want to read the file
-    const coverageFile = fs.readFileSync(coverageSummary, 'utf8');
-    console.log(coverageFile);
-    const json = JSON.parse(coverageFile);
+    const json = JSON.parse(fs.readFileSync(coverageSummary, 'utf8'));
     const summary =
         `All files: \n Statements: ${json.total.statements.pct}%, ` +
         `Branches: ${json.total.branches.pct}%, ` +
         `Functions: ${json.total.functions.pct}%, ` +
         `Lines: ${json.total.lines.pct}%`;
 
+    const request = require('request');
+    const url = `https://api.github.com/gists/${gistId}`; //
+    const package = require("./package.json");
+    const reportFilename = package.name + '.txt';
+    const options = {
+        url: url,
+        method: 'patch',
+        auth: {
+            user: gitHubUser,
+            password: accessToken
+        },
+        headers: {
+            'User-Agent': 'Github-Activity'
+        },
+        body: {
+            description: 'Code Coverage Reports',
+            files: { reportFilename: { filename: reportFilename, content: summary } }
+        },
+        json: true
+    };
+    request(options, function(error, response, body) {
+        if (error) {
+            console.log(error)
+            return
+        }
+        console.log(`Sending coverage report to Gist: (https://gist.github.com/${gitHubUser}/${gistId})`);
+        console.log('Status Code', response.statusCode)
+    });
 
     console.log("Summary", summary);
 
